a0cd9f938aa6583915dcec0b14da6683fd29be02
Moinul Islam Raj <moinulislamraj0@gmail.com>
2024-03-12 15:40:38 +0600

    image sending feature

--- scrypt.js
+++ scrypt.js
@@ -4,10 +4,16 @@ const INPUT = document.getElementById("input")
 const INPUT2 = document.getElementById("name")
 const SUBMIT_BTN = document.getElementById("submit")
 const form = document.getElementById("form")
+const FILE_INPUT = document.getElementById("file")
+const LABEL = document.getElementById("label")
 
 const url = "https://messages-api-v5bk.onrender.com/api/messages"
 
 INPUT2.value = localStorage.getItem("name") || ""
+FILE_INPUT.onchange = () => {
+    const arr = FILE_INPUT.value.split("\\");
+    LABEL.innerText = arr[arr.length-1];
+}
 
 const update =() => {
     localStorage.setItem("name",INPUT2.value)
@@ -17,17 +23,66 @@ const update =() => {
         let str=""
         for(let i=0;i<res.length;i++){
             const date = new Date(res[i].createdAt)
-            str+= `
-            <div class="message ${res[i].username == INPUT2.value.toUpperCase() ? "right" : "left"}">
-                <p>${res[i].text}</p>
-                <div class="details">
-                    <p class="username">${res[i].username}</p>
-                    <p class="date">${date.toLocaleDateString()+" - "+date.toLocaleTimeString()}</p>
-                </div>
-            </div>`
+            const type = res[i].messageType
+            if (type == "text") {
+                str+= `
+                <div class="message ${res[i].username == INPUT2.value.toUpperCase() ? "right" : "left"}">
+                    <p>${res[i].text}</p>
+                    <div class="details">
+                        <p class="username">${res[i].username}</p>
+                        <p class="date">${date.toLocaleDateString()+" - "+date.toLocaleTimeString()}</p>
+                    </div>
+                </div>`
+            }
+            else if (type == "image") {
+                str+= `
+                <div class="message ${res[i].username == INPUT2.value.toUpperCase() ? "right" : "left"}">
+                    <p>${res[i].text}</p>
+                    <img src="${res[i].fileData}" alt="image failed">
+                    <div class="details">
+                        <p class="username">${res[i].username}</p>
+                        <p class="date">${date.toLocaleDateString()+" - "+date.toLocaleTimeString()}</p>
+                    </div>
+                </div>`
+            }
         }
         MESSAGES.innerHTML=str
     }); 
+}
+
+const post = (username, text, fileData, type) => {
+    if (type == "text") {
+        if(text == null || text == "") return
+        if(username == null || username == "") username = "unknown"
+    
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({ text, username }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            }
+        })
+        .then(() => update());
+    }
+    else if(type == "image"){
+        if(text == null || text == "") text = "Image"
+        if(username == null || username == "") username = "unknown"
+        
+        
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({ text, username, fileData, messageType:"image" }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            }
+        })
+        .then(res => {
+            update()
+        })
+        .catch((e) => {
+            alert("file too big")
+        });
+    }
 }
 
 update();
@@ -36,18 +91,24 @@ setInterval(update,3000);
 form.onsubmit = () => { 
     const text = INPUT.value
     let username = INPUT2.value.toUpperCase()
+    let file = FILE_INPUT.files[0] || null;
     INPUT.value=""
-    if(text == null || text == "") return false
-    if(username == null || username == "") username = "unknown"
+    FILE_INPUT.value = null;
+    LABEL.innerText = "Image"
 
-    fetch(url, {
-        method: 'POST',
-        body: JSON.stringify({ text, username }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8'
+
+    if (!file) {
+        post(username, text, null, "text")
+    }
+    else{
+        const fr = new FileReader();
+        fr.readAsDataURL(file);
+
+        fr.onload = () => {
+            post(username, text, fr.result, "image")
         }
-    })
-    .then(() => update());
+    }
+    
     return false
 }
 
